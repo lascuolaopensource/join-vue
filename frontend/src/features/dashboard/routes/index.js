c6d708bfaf781f399d07cd360ec724fbaf03ee06
flavio2018 <flavio.petruzzellis@gmail.com>
2021-07-16 11:27:59 +0200

    Define user profile as Dashboard sub-route

--- frontend/src/features/dashboard/routes/index.js
+++ frontend/src/features/dashboard/routes/index.js
@@ -1,9 +1,13 @@
 import Dashboard from '../views/DashboardIndex.vue';
+import UserProfile from '../../profile/routes';
 
 export default [
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard,
+    children: [
+      ...UserProfile,
+    ],
   },
 ];
