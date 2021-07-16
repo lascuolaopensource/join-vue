28ce49cfa3e4959b1eb9061340cf142b5822cddf
flavio2018 <flavio.petruzzellis@gmail.com>
2021-07-16 11:29:34 +0200

    Remove user profile route from main router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,14 +3,12 @@ import VueRouter from 'vue-router';
 
 import Home from '../features/home/routes';
 import LoginWindow from '../features/login/routes';
-import UserProfile from '../features/profile/routes';
 
 Vue.use(VueRouter);
 
 const routes = [
   ...Home,
   ...LoginWindow,
-  ...UserProfile,
 ];
 
 const router = new VueRouter({
